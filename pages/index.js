69367cf39ed431752a3d1fcec6077346c4b16f5f
AlthofZ <althof.edu@gmail.com>
2024-05-09 13:23:36 +0700

    Styled Components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import RestaurantList from "../components/RestaurantList";
+import Container from "../components/styled/Container";
 
 export default function Home({ restaurants }) {
-  return <div className="container">
-    <RestaurantList restaurants={restaurants} />
-  </div>;
+  return (
+    <Container>
+      <RestaurantList restaurants={restaurants} />
+    </Container>
+  );
 }
 
-export async function getStaticProps({params}) {
-  const response = await fetch('https://restaurant-api.dicoding.dev/list');;
+export async function getStaticProps({ params }) {
+  const response = await fetch("https://restaurant-api.dicoding.dev/list");
   const { restaurants } = await response.json();
   return {
     props: {
       restaurants,
     },
-  }
+  };
 }
